58d576e1988f7b35c6e2b3efdcd4bab76d254a90
IrsyadHalimi <irsyadhalimi08@gmail.com>
2024-12-20 15:04:37 +0700

    Project initialization, connect to MongoDB

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+
+require('dotenv').config();
+require('./libs/dbConnect');
+
+const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.render('index', { message: 'Hello From Node.js' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('index', { message: 'The Contact Page' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('index', { message: 'The About Page' });
+});
+
+app.get('*', (req, res) => {
+  res.status(404).render('index', { message: 'Not Found' });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
